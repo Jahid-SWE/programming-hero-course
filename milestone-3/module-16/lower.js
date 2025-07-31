ca2b6c0274b270812576841eef859eb507d859f6
jahid Hassan <dev.jahidhassan@gmail.com>
2025-07-31 23:47:34 +0600

    toLowerCass() and trim() clear

---
+++ milestone-3/module-16/lower.js
@@ -0,0 +1,24 @@
+// toLowerCase() function babohar kora hoi sudu matro case sensitive korar jonno
+
+// let college = 'Dhaka';
+// let university = 'dhaka';
+// if(college.toLowerCase()=== university.toLowerCase()){
+//     console.log('dhaka university student Here');
+// }
+// else{ 
+//     console.log(' Here all student of daffodil International University');
+// }
+
+
+// trim() function babohar kora hoi sutu word er samne abong sashe space control korar jonno
+
+let food  = ' Birani  ';
+let favirateFood = 'Birani    ';
+
+if(food.trim()===favirateFood.trim()){
+    console.log(food);
+
+}
+else{
+    console.log('bad food')
+}
---
+++ milestone-3/module-16/object.js
@@ -0,0 +1,16 @@
+const name = 'jahid';
+const friend = "polash";
+const fruit =`mango`;
+console.log(typeof fruit);
+
+// array is mutable --> it can  change value index wise
+let age = [25,64,35,48,78];
+console.log(age.length);
+age[2]=10;
+console.log(age);
+
+
+// string is immutable --> not changeable string
+let myName= 'hasnat';
+myName[0]='P'
+console.log(myName)
