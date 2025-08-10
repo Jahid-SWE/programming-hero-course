93f0c214bed0bfbdb57f3ae28672366625b6550d
jahid Hassan <dev.jahidhassan@gmail.com>
2025-08-10 17:00:39 +0600

    copy formula used here in array

---
+++ milestone-4/module-18/arrayofarray.js
@@ -0,0 +1,19 @@
+let marks = [
+    [91,64,19,73,78],
+    [97,19,82,38],
+    [28,64,76,17,91]
+]
+// console.log(marks);
+// console.log(marks[2]);
+// console.log(marks[2][1]);
+// marks[2][1]=100;
+// console.log(marks[2][1]);
+// console.log(marks)
+// marks[0].push(99)
+// marks[0].shift()
+// console.log(marks)
+
+
+for(let mark of marks){
+    console.log(mark);
+}
--- milestone-4/module-18/copy.js
+++ milestone-4/module-18/copy.js
@@ -1,4 +1,70 @@
-let numberone = [20,84,76,94,74]
-let refnumber = numberone;
-refnumber[0]= 80;
-console.log(refnumber)
+
+//  primitive data 
+/**
+ * let price =25;
+let compPrice =price;
+console.log(price);
+compPrice= 20;
+console.log(compPrice);
+
+ */
+
+// non-primitive data kuje arrat object
+/**
+ * const marks =[20,28,64,87,74,75]
+const comMarks = marks;
+
+comMarks[1]=50;
+console.log(marks);
+console.log(comMarks);
+ */
+
+
+// fokira system copy korar akta array theike arak ta array te
+
+/**
+ * let carSpeeds =[60,64,84,27,54,90];
+let bmdCarSpeed =[];
+
+for(let carSpeed of carSpeeds){
+    bmdCarSpeed.push(carSpeed);
+}
+
+carSpeeds[1]=100;
+bmdCarSpeed[0]=110;
+console.log(carSpeeds);
+console.log(bmdCarSpeed);
+ */
+
+
+
+//Short cut array copy system using Array.from()
+
+/**
+ * const numbers = [1,2,3,4];
+const newNumbers= Array.from(numbers);
+numbers.push(189)
+console.log(numbers);
+console.log(newNumbers);
+ */
+
+
+// short cut using new array concat()
+/**
+ * const numbers=  [1,2,3,4];
+const newNumbers=[].concat(numbers);
+
+newNumbers.push(15)
+console.log(numbers);
+console.log(newNumbers);
+ */
+
+
+//  ... dot diye number change kora 
+const numbers =[1,2,3,4];
+const newNumbers =[...numbers];
+
+newNumbers.push(80);
+console.log(numbers)
+console.log(newNumbers)
+
