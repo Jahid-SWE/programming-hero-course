970ff768958335c5dabf112b0abf4a9f32754921
jahid Hassan <dev.jahidhassan@gmail.com>
2025-08-10 11:17:12 +0600

    array of object cear here

---
+++ milestone-4/module-18/arrayofobject.js
@@ -0,0 +1,43 @@
+// const numbers = [20,91,3,76,75,14];
+// let employees =[
+//     {name:'kamlal',designation:'Hr', salary: 25000},
+//     {name:'hasnat',designation:'Fontend developer', salary: 75000},
+//     {name:'atik',designation:'backend developer', salary: 75000}
+// ]
+// console.log(employees);
+// console.log(typeof employees);
+
+
+// let product=[
+//     {name:'Kajol',
+//         id:75,
+//         price:58555
+//     },
+//     {name:'hasnat',
+//         id:71,
+//         price:5111
+//     },
+//     {name:'jahid',
+//         id:10,
+//         price:900
+//     }
+// ]
+// console.log(product);
+// console.log(product[1]);
+
+let friends = [
+
+{name:'Faysal', age: 20, single:'yes', job:'dhaka', salary:2000},
+{name:'Towhid', age: 52, single:'no', job:'khulna', salary:100000},
+{name:'AbdurRahman', age: 42, single:'yes', job:'khulna', salary:30000},
+{name:'cuddus', age: 82, single:'no', job:'rajshahi', salary:40000},
+]
+
+// console.log(friends[2].name);
+// friends[2].name= 'kusbo';
+// console.log(friends[2].name)
+for(friend of friends){
+    console.log(friend.name, friend.age);
+}
+
+
